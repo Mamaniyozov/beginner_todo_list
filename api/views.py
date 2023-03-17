3b71f62d07fc9f0cb726e7dce86bf2ea8d396218
Mmaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-03-17 10:16:03 +0500

    add task

--- api/views.py
+++ api/views.py
@@ -15,7 +15,7 @@ def get_all_task(request:HttpRequest):
             result.setdefault(i,[])
             result[i].append({          
                     'id':i.id,
-                    "task":i.task,
+                    "task":name,
                     'created_at':i.created_at,
                     'updated_at':i.updated_at
                     
@@ -31,4 +31,43 @@ def delete_task(reqeust: HttpRequest, pk: int) -> JsonResponse:
             return JsonResponse(product.to_dict())
         except ObjectDoesNotExist:
             return JsonResponse({"status": "object doesn't exist"})
+def get_update_task(request: HttpRequest, pk: int):
+     if request.method=='GET':
+        # Get the smartphone object
+        tas = Todolist.objects.get(id=pk)
+        # Get the price from the request
+        price = tas.id
+        # Update the price
+        tas.task= price
+        tas.save
+        return JsonResponse({
+            "task":tas.task,
+            "created_at":tas.created_at,
+            "updated_at":tas.updated_at
+        })
+def add_task(reqeust: HttpRequest) -> JsonResponse:
+    """add new product to database"""
+    if reqeust.method == 'POST':
+        # get body from request
+        body = reqeust.body
+        # get body data
+        decoded = body.decode()
+        # data to dict
+        data = json.loads(decoded)
+        task= data.get("task",False)
+        task_text=data.get("task_text",False)
+        if task == False:
+            return JsonResponse({"status": "price field is required."})
+        if task_text == False:
+            return JsonResponse({"status": "price field is required."})
+        fix=Todolist()
+        fix.task=task
+        fix.task_text=task_text
+        fix.save
+
+
+
+        return JsonResponse({'result':data})
+    
+    return JsonResponse({})
 # Create your views here.
